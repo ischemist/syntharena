8f2cfdecc45667c7c2dae571a4090427409a2eb6
Anton Morgunov <anton.morgunov@yale.edu>
2025-11-24 12:48:14 -0500

    dev: dark mode support

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image'
 
+import { ModeToggle } from '@/components/mode-toggle'
+
 export default function Home() {
     return (
         <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
@@ -9,6 +11,7 @@ export default function Home() {
                     <h1 className="max-w-xs text-3xl leading-10 font-semibold tracking-tight text-black dark:text-zinc-50">
                         To get started, edit the page.tsx file.
                     </h1>
+                    <ModeToggle />
                     <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
                         Looking for a starting point or more instructions? Head over to{' '}
                         <a
