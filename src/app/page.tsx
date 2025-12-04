7d2f814c65db5db5b9572a8c6b1b8ed2f546646c
Anton Morgunov <anton.morgunov@yale.edu>
2025-12-03 19:42:23 -0500

    ref: consistent structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import { Suspense } from 'react'
 
-import { HeaderSection } from './_components/home/header-section'
-import { NavigationSection } from './_components/home/navigation-section'
-import { ProblemSolutionSection } from './_components/home/problem-solution-section'
-import { BenchmarksOverviewSection } from './_components/home/server/benchmarks-overview-section'
-import { LiveStatsSection } from './_components/home/server/live-stats-section'
-import { BenchmarksTableSkeleton, StatsTableSkeleton } from './_components/home/skeletons'
+import { BenchmarksOverviewSection } from './_components/server/benchmarks-overview-section'
+import { HeaderSection } from './_components/server/header-section'
+import { LiveStatsSection } from './_components/server/live-stats-section'
+import { NavigationSection } from './_components/server/navigation-section'
+import { ProblemSolutionSection } from './_components/server/problem-solution-section'
+import { BenchmarksTableSkeleton, StatsTableSkeleton } from './_components/skeletons'
 
 export default function Home() {
     return (
