84460dddea6ea31a4d693f41614430955bba1fb1
Anton Morgunov <anton.morgunov@yale.edu>
2025-12-10 19:34:39 -0500

    dev: metrics page

---
+++ src/app/@breadcrumbs/docs/metrics/page.tsx
@@ -0,0 +1,28 @@
+import Link from 'next/link'
+
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb'
+
+export default function MetricsBreadcrumb() {
+    return (
+        <Breadcrumb>
+            <BreadcrumbList>
+                <BreadcrumbItem>
+                    <BreadcrumbLink asChild>
+                        <Link href="/docs">Docs</Link>
+                    </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator />
+                <BreadcrumbItem>
+                    <BreadcrumbPage>Metrics</BreadcrumbPage>
+                </BreadcrumbItem>
+            </BreadcrumbList>
+        </Breadcrumb>
+    )
+}
