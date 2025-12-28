13ed74b481a45e07be26cf66a3565b2da3ed7b82
Anton Morgunov <anton.morgunov@yale.edu>
2025-12-28 16:15:26 -0500

    dev: remove redundant check

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -15,9 +15,6 @@ export function formatError(error: unknown): string {
     if (error instanceof Error) {
         return error.message
     }
-    if (typeof error === 'string') {
-        return error
-    }
     return String(error)
 }
 
